0a67fcdaac731b377afaed8c852aea2c936daaf1
Komal Jaiswal <komaljaiswal2810@gmail.com>
2023-10-08 16:47:46 +0530

    basic setup

---
+++ amal/lib/constant/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // App Colors
+  static const amalGreen = Color(0xffC6F267);
+  static const amalBlack = Color(0xff171717);
+  static const amalLightBlack = Color(0xff2e2e2e);
+}
