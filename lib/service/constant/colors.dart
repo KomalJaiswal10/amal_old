f95b4b0dd8c47f290c2e423bce59b5a2d8482307
komalAnIntl <komal@an-intl.co>
2023-11-10 18:44:38 +0530

    color

--- lib/service/constant/colors.dart
+++ lib/service/constant/colors.dart
@@ -4,7 +4,8 @@ class AAppColors {
   AAppColors._();
 
   // App Colors
-  static const Color amalGreen = Color(0xffC6F267);
+  static const Color amalGreen2 = Color(0xffC6F267);
+  static const Color amalGreen = Color(0xffC9F66F);
   static Color amalBlack = const Color(0xff171717).withOpacity(.8);
   static Color amalLightBlack = const Color(0xff2e2e2e).withOpacity(.7);
 
